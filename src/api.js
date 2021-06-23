9f8659873e7a3625c266990c48982a5d2174dd64
Rogan-Templer <rmtempler@gmail.com>
2021-06-24 10:44:35 +1200

    api.js created

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import request from 'superagent'
+
+export function getDadJokes() {
+  return request.get('https://icanhazdadjoke.com/')
+    .set('Accept', 'application/json')
+    .then(response => {
+      return response.body.joke
+})
+}

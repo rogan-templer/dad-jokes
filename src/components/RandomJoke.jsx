536778bd615b38cbe7d1d1994f899f08b9a1c4be
Rogan-Templer <rmtempler@gmail.com>
2021-09-20 21:19:30 +1200

    the styling continues

--- src/components/RandomJoke.jsx
+++ src/components/RandomJoke.jsx
@@ -14,15 +14,17 @@ function RandomJoke() {
 
   return (
     <>
-      <div>
-        <div>
-          <h1>Jokes to make your dad proud!</h1>
-        </div>
-        <div>
-          <h2>{dadJoke}</h2>
-        </div>
-        <div className="center">
-          <button onClick={refreshPage}>New Joke Please!</button>
+      <div className="grid">
+        <div className="col joke-container">
+          <div className="col heading">
+            <h1>Jokes to make your dad proud!</h1>
+          </div>
+          <div className="col joke">
+            <h2>{dadJoke}</h2>
+          </div>
+          <div className="col center">
+            <button onClick={refreshPage}>Gimme another...</button>
+          </div>
         </div>
         <br />
       </div>
